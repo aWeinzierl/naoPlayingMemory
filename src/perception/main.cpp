bb98b61b27e0ee0c95670a3ec0b9886a9b844fb8
ge25quv <francois.le-breton@tum.de>
2019-06-25 17:59:33 +0200

    [WIP] implement speech recognition

--- src/perception/main.cpp
+++ src/perception/main.cpp
@@ -2,50 +2,19 @@
 #include <naoqi_bridge_msgs/SetSpeechVocabularyActionGoal.h>
 #include <naoqi_bridge_msgs/SpeechWithFeedbackActionGoal.h>
 #include <std_srvs/Empty.h>
+#include "SpeechRecognition.h"
 
 main(int argc, char **argv) {
-    /*ros::init(argc, argv, "perception");
+   ros::init(argc, argv, "perception");
+   ros::NodeHandle n;
 
-    //Speech Recognition
-    ros::NodeHandle n;
-    ros::Rate r_sleep(30);
-    ros::Publisher vocab_pub = n.advertise<naoqi_bridge_msgs::SpeechVocabularyActionGoal>("/speech_vocabulary_action/goal",1);
-    ros::ServiceClient start_reco_srv = n.serviceClient<std_srvs::Empty>("/start_recognition");
-    ros::ServiceClient stop_reco_srv = n.serviceClient<std_srvs::Empty>("/stop_recognition");
+   std::vector<std::string> available_vocabulary;
+   available_vocabulary.push_back("Yes");
+   available_vocabulary.push_back("No");
+   available_vocabulary.push_back("Hi Nao");
+   std::string result;
+   uint32_t duration=20;
 
-
-    while(!chatter_pub.getNumSubscribers()){
-            r_sleep.sleep();
-                        };
-
-    */
-    //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("speech",1);
-    //while(!chatter_pub.getNumSubscribers()){
-    //    r_sleep.sleep();
-    //}
-    //std_msgs::String msg;
-
-    //msg.data="Hi my friend, Lets play a round of memory!";
-
-    //chatter_pub.publish(msg);
-
-    //msg.data="I will start, ok?";
-
-    //chatter_pub.publish(msg);
-
-    //"word_recognized"
-
-    //"rosrun nao_playing_memory action \n"
-    //"cogr@stud04:~/ros/workspace/memory$ rostopic echo /word_recognized \n"
-    //"^Ccogr@stud04:~/ros/workspace/memory$ rostopic info /word_recognized \n"
-    //"Type: naoqi_bridge_msgs/WordRecognized\n"
-    //"\n"
-    //"Publishers: \n"s
-    //" * /nao_speech (http://stud04:40378/)\n"
-    //"\n"
-    //"Subscribers: None\n"
-    //""
-
-    //"/word_recognized"
-    //"rosservice call /start_recognition "{}""
+   perception::SpeechRecognitionClient RecogClient(n);
+   RecogClient.listen(available_vocabulary,result, duration);
 }
