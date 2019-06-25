63035974d1a1417b1ad4089752038ad2085f04d4
Andreas Weinzierl <20560137+aWeinzierlo@users.noreply.github.com>
2019-06-25 14:51:29 +0200

    put say something in different function

--- src/action/main.cpp
+++ src/action/main.cpp
@@ -1,57 +1,30 @@
+#include <string>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <zconf.h>
 
+namespace SPEECH {
+    void say_something(std::string text_to_say){
+
+        ros::NodeHandle n;
+        ros::Publisher chatter_pub = n.advertise<std_msgs::String>("speech",1);
+        ros::Rate r_sleep(20);
+        while(!chatter_pub.getNumSubscribers()){
+            r_sleep.sleep();
+        }
+
+        std_msgs::String msg;
+        msg.data = text_to_say;
+
+        chatter_pub.publish(msg);
+    }
+}
 
 main(int argc, char **argv) {
     ros::init(argc, argv, "action");
 
-    ros::NodeHandle n;
-    ros::Rate r_sleep(10);
-
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("speech",1);
-    while(!chatter_pub.getNumSubscribers()){
-        r_sleep.sleep();
-    }
-    std_msgs::String msg;
-
-    msg.data="Hi my friend, Lets play a round of memory!";
-
-    chatter_pub.publish(msg);
-
-    msg.data="I will start, ok?";
-
-    chatter_pub.publish(msg);
-
-    "word_recognized"
-
-    "rosrun nao_playing_memory action \n"
-    "cogr@stud04:~/ros/workspace/memory$ rostopic echo /word_recognized \n"
-    "^Ccogr@stud04:~/ros/workspace/memory$ rostopic info /word_recognized \n"
-    "Type: naoqi_bridge_msgs/WordRecognized\n"
-    "\n"
-    "Publishers: \n"
-    " * /nao_speech (http://stud04:40378/)\n"
-    "\n"
-    "Subscribers: None\n"
-    ""
-
-    "/word_recognized"
-    "rosservice call /start_recognition "{}""
-
-
-
-
-    ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<std_msgs>("start_recognition");
-    beginner_tutorials::AddTwoInts srv;
-    srv.request.a = atoll(argv[1]);
-    srv.request.b = atoll(argv[2]);
-    if (client.call(srv))
-
-
-
-
-
+    std::string text;
+    text = "Hello";
+    SPEECH::say_something(text);
 
 }
