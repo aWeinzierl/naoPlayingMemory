3a18e972453d3d2e7e90f8b48f11a92af7278166
ge25quv <francois.le-breton@tum.de>
2019-06-29 17:14:31 +0200

    [WIP] initialize the grid

--- src/perception/Vision.cpp
+++ src/perception/Vision.cpp
@@ -34,15 +34,17 @@ perception::VisionClient::VisionClient(ros::NodeHandle &nodeHandle):it_(nodeHand
     cameraP.at<float>(2,1)=0.000000;
     cameraP.at<float>(2,2)=1.000000;
 
-    TheCameraParameters.setParams(cameraP,dist,cv::Size(640,480));
-    TheCameraParameters.resize(cv::Size(640,480));
+    CameraParameters.setParams(cameraP,dist,cv::Size(640,480));
+    CameraParameters.resize(cv::Size(640,480));
 
     position_thresh = 0.08;
 
-    old_positions = cv:: Mat::zeros(cv::Size(255,5), CV_32FC1);
-    new_positions = cv::Mat::zeros(cv::Size(255,5), CV_32FC1);
+    card_positions = cv:: Mat::zeros(cv::Size(255,5), CV_32FC1);
+    game_grid = cv:: Mat::zeros(cv::Size(3,4), CV_32FC1);
 
     timepoint = 0;
+
+    game_initialized = false;
 }
 
 void perception::VisionClient::image_callback(const sensor_msgs::ImageConstPtr &msg) {
@@ -64,12 +66,68 @@ void perception::VisionClient::image_callback(const sensor_msgs::ImageConstPtr &
 }
 
 void perception::VisionClient::detect_cards() {
-    markerDetector.detect(InImage, markers, TheCameraParameters);
+    markerDetector.detect(InImage, markers, CameraParameters);
 
-    for(auto& marker : markers) {
-        marker.draw(InImage, cv::Scalar(0,0,255),2);
+    if(game_initialized==false){
+        _initialize_game_grid();
     }
+
 
     imshow("markers",this->InImage);
     cv::waitKey(10);
+}
+
+void perception::VisionClient::_initialize_game_grid() {
+
+    for(auto& marker : markers) {
+        marker.draw(InImage, cv::Scalar(0,0,255),2);
+        marker.calculateExtrinsics(0.06, CameraParameters, true);
+
+        for(int i=0; i<3;++i){
+            card_positions.at<float>(i,marker.id) = marker.Tvec.at<float>(i);
+        }
+    }
+
+    std::cout<<"Card positions: "<<card_positions<<std::endl;
+    int i;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            int position = 0;
+            position = i+j;
+            game_grid.at<float>(i,j) = _get_card_position(position);
+        }
+    }
+
+
+}
+
+int perception::VisionClient::_get_card_position(int number) {
+    switch(number){
+        case 0:
+            int min_x, min_y;
+
+            min_x = _find_min_position(card_positions);
+    }
+}
+
+int perception::VisionClient::_find_min_position(cv::Mat matrix) {
+    int id = 0;
+    int num_rows;
+    float min_val = 10000.0;
+
+    num_rows = matrix.rows;
+    for(int col = 0; col<2; ++col){
+        float curr_val = 0.0;
+
+        for(int row=0; row<num_rows; ++row) {
+            curr_val = matrix.at<float>(row, col);
+            if (!curr_val == 0) {
+                if (curr_val < min_val) {
+                    min_val = curr_val;
+                    id = row;
+                }
+            }
+        }
+    }
+    return id;
 }
--- src/perception/Vision.h
+++ src/perception/Vision.h
@@ -17,13 +17,13 @@ namespace perception{
         image_transport::Subscriber image_sub_;
         image_transport::Publisher image_pub_;
 
-        aruco::CameraParameters TheCameraParameters;
+        aruco::CameraParameters CameraParameters;
         aruco::MarkerDetector markerDetector;
         std::vector<aruco::Marker>markers;
 
         cv::Mat InImage;
-        cv::Mat old_positions;
-        cv::Mat new_positions;
+        cv::Mat card_positions;
+        cv::Mat game_grid;
 
         json_prolog::Prolog pl;
 
@@ -33,9 +33,17 @@ namespace perception{
         int timepoint;
         float position_thresh;
 
+        bool game_initialized;
+
     public:
         explicit VisionClient(ros::NodeHandle& nodeHandle);
         void image_callback(const sensor_msgs::ImageConstPtr& msg);
         void detect_cards();
+
+    private:
+        void _initialize_game_grid();
+        int _get_card_position(int number);
+        int _find_min_position(cv::Mat matrix);
+        int _fing_max_position(cv::Mat matrix);
     };
 }
